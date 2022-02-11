0384454371892631c3dce90a94ab71b5e477b6d0
BAaboe <benjamin.mjaatvedt@gmail.com>
2022-02-11 17:38:19 +0000

    We got a window, WHOOOO

---
+++ src/Game.py
@@ -0,0 +1,31 @@
+import pygame
+from pygame.locals import *
+from src import Window
+
+pygame.init()
+
+class Game:
+    def __init__(self):
+        self.wd = Window.Window()
+
+        self.FPS = 30
+        self.FramesPerSecond = pygame.time.Clock()
+
+        self.wd.window.fill(self.wd.BLACK)
+
+    def game_loop(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    quit()
+            self.wd.window.fill(self.wd.BLACK)
+
+
+            pygame.display.update()
+            self.FramesPerSecond.tick(self.FPS)
+
+
+    def start(self):
+        #Do some shit idk
+        self.game_loop()
